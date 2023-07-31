18cccd68e46ad24fb91fde190822ecdb973fabac
fanjc <416233876@qq.com>
2023-07-31 14:30:42 +0800

    拱呀拱web测试

---
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -0,0 +1,13 @@
+System.register(['./instantiated-007af047.js'], function (exports) {
+	'use strict';
+	return {
+		setters: [function (module) {
+			exports('default', module.i4);
+		}],
+		execute: function () {
+
+
+
+		}
+	};
+});
